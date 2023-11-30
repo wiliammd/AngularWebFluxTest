46e9859dc68c68ff565e7ac0eca4da68060addc8
wiliam <wiliam.depiro@wifeed.com.br>
2023-11-30 19:21:46 -0300

    pequeno ajuste formatacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import { WebSocketAPI } from './WebSocketAPI';
+import { Component } from "@angular/core";
+import { WebSocketAPI } from "./WebSocketAPI";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   message: string;
   nome: string = null;
   temNome = false;
   receivedMessages: any[] = [];
-  constructor(private webSocketService: WebSocketAPI) { }
+  constructor(private webSocketService: WebSocketAPI) {}
 
   ngOnInit() {
     this.webSocketService.initializeWebSocketConnection();
@@ -20,19 +20,25 @@ export class AppComponent {
     });
   }
 
-  iniciarConexao(){
-    if(this.nome !== undefined && this.nome !== null ){
+  iniciarConexao() {
+    if (this.nome !== undefined && this.nome !== null) {
       this.receivedMessages = [];
       this.webSocketService.conectarComNome(this.nome);
       this.temNome = true;
-    }else{
-      alert('Por favor, insira seu nome')
+    } else {
+      alert("Por favor, insira seu nome");
     }
   }
 
   sendMessage() {
-    this.webSocketService.enviarMensagem({sender:this.nome, content: this.message ,type:"CHAT"});
-    this.message = '';
+    if (this.message !== undefined && this.message !== null && this.message !== "") {
+      this.webSocketService.enviarMensagem({
+        sender: this.nome,
+        content: this.message,
+        type: "CHAT",
+      });
+      this.message = "";
+    }
   }
 
   //funcao para gerar cor aleatorio de acordo com nome
@@ -42,15 +48,17 @@ export class AppComponent {
     for (let i = 0; i < nome.length; i++) {
       hash = nome.charCodeAt(i) + ((hash << 5) - hash);
     }
-  
+
     // Componentes RGB baseados no hash
-    const r = (hash & 0xFF0000) >> 16;
-    const g = (hash & 0x00FF00) >> 8;
-    const b = hash & 0x0000FF;
-  
+    const r = (hash & 0xff0000) >> 16;
+    const g = (hash & 0x00ff00) >> 8;
+    const b = hash & 0x0000ff;
+
     // Construir a cor no formato hexadecimal
-    const cor = `#${(1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)}`;
-  
+    const cor = `#${((1 << 24) | (r << 16) | (g << 8) | b)
+      .toString(16)
+      .slice(1)}`;
+
     return cor;
   }
 }
